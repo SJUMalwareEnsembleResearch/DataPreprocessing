db70f2c51c56027dd1f30dfb1632745b1b710c2d
Gavin Wong <gavinmwong@gmail.com>
2020-06-18 10:58:52 -0700

    old

--- extract.py
+++ extract.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 12 14:55:24 2020
-
-@author: gavinwong
-"""
-
 import r2pipe
 from os import listdir
 import os
@@ -17,25 +9,31 @@ def iterate(foldername):
 ##any command that runs in the r2 shell can be pased to the cmd method
 def extract_write(filename):
     r = r2pipe.open(filename)
-    s = r.cmd("pD 64")
+    s = r.cmd("p8 64")
     
     ##creating new text file
     slash = filename.rfind("/")
     txt_file = filename[slash + 1:] + ".txt"
-    directory = "/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/"
+    directory = "/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/ByteSequences/"
     print(txt_file)
     
     f = open(os.path.join(directory, txt_file), "w+")
-    f.write("DOS Header")
-    f.write(s)
+    f.write("DOS Header\n")
+    f.write(s+"\n")
+    f.write("PE Header\n")
+
+    byteSpecfied = r.cmd("px 1 @ 0x3c")
+    hexStart = byteSpecfied.rfind("0x")
+    print(byteSpecfied)
+    byteSpecfied = byteSpecfied[hexStart + 12:]
+    pe = r.cmd("p8 264 @0x"+byteSpecfied)
+    f.write(pe)
+    print(byteSpecfied)
+
+
     f.close()
     return(str(directory + txt_file))
 
 
 if __name__ == "__main__":
-    s = extract_write("/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/VirusShare_fffb1996a5b7c4c716931af2842712e3")
-    print(s)
-    
-    
-    
-            
+    s = (extract_write("/Users/gavinwong/Desktop/v001/VirusShare_fffb1996a5b7c4c716931af2842712e3"))
