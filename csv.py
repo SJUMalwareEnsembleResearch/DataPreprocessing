f5010e33da52ee4646c23417849b2840c5b9ee03
Gavin Wong <gavinmwong@gmail.com>
2020-06-23 17:03:54 -0700

    grab csv

---
+++ csv.py
@@ -0,0 +1,14 @@
+import csv
+
+fields = ["id", "family", "amount"]
+for i in range 1000:
+	string = "op" + str(i)
+	fields.append(string)
+
+filename = "opcodes.csv"
+
+dataset = []
+for ()
+
+with open (filename, 'w') as csvfile:
+
